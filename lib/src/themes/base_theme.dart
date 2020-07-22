5b440eb6603317025761ea61ef644e38ca033f7e
Janusz Dziedzic <janusz.dziedzic@mirasol.media>
2020-07-22 20:10:39 +0200

    Added temporary EDK logo, adjusted basic colors according to designs

--- lib/src/themes/base_theme.dart
+++ lib/src/themes/base_theme.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static final edkRed = Color(0xffe30a17);
+  static final edkRed = Color(0xFFE30A17);
 
-  static final bottomNavigationBarBackgroundColor = Color(0xff151719);
-  static final bottomNavigationBarSelectedColor = Color(0xffffffff);
-  static final bottomNavigationBarUnselectedColor = Color(0xff657790);
+  static final bottomNavigationBarBackgroundColor = Color(0xFF121519);
+  static final bottomNavigationBarSelectedColor = Color(0xFFF6F7FA);
+  static final bottomNavigationBarUnselectedColor = Color(0xFF52606D);
 
   static ThemeData get theme {
     return ThemeData.dark().copyWith(
       primaryColor: edkRed,
-      scaffoldBackgroundColor: Color(0xff212326),
+      scaffoldBackgroundColor: Color(0xFF1B1E22),
     );
   }
 }
